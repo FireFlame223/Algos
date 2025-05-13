75e55863696f8fd5f459d324484e8bfebd3fe75a
Infernich <pentagonvisdax@gmail.com>
2025-05-13 14:47:12 +0200

    Warm up 4
    Multiple splits

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -6,18 +6,24 @@ public class DungeonGenerator : MonoBehaviour
 
     void Update()
     {
-        RectInt room1, room2;
+        RectInt room1, room2, room3, room4;
         if (splitHorizontally)
         {
             room1 = new RectInt(0, 0, 100, 25);
             room2 = new RectInt(0, 24, 100, 26);
+            room3 = new RectInt(0, 0, 50, 25);
+            room4 = new RectInt(49, 0, 51, 25);
         }
         else
         {
             room1 = new RectInt(0, 0, 50, 50);
             room2 = new RectInt(49, 0, 51, 50);
+            room3 = new RectInt(0, 0, 50, 25);
+            room4 = new RectInt(0, 24, 50, 26);
         }
         AlgorithmsUtils.DebugRectInt(room1, Color.green);
         AlgorithmsUtils.DebugRectInt(room2, Color.blue);
+        AlgorithmsUtils.DebugRectInt(room3, Color.yellow);
+        AlgorithmsUtils.DebugRectInt(room4, Color.red);
     }
 }
