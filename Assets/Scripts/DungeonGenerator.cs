7891642bdc1957a825e16cfecb9fb6a20c563fc7
Infernich <pentagonvisdax@gmail.com>
2025-05-13 16:38:33 +0200

    First attempt to randomize room parameters + graph base

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class DungeonGenerator : MonoBehaviour
 {
     public int minRoomSize = 25;
+    public int maxRooms = 4;
 
     private RectInt room1, room2, room3, room4;
 
@@ -13,14 +14,51 @@ public class DungeonGenerator : MonoBehaviour
 
     void GenerateRooms()
     {
-        int width1 = Random.Range(minRoomSize, 100);
-        int width2 = Random.Range(minRoomSize, 100);
-        int height1 = Random.Range(minRoomSize, 50);
-        int height2 = Random.Range(minRoomSize, 50);
-        room1 = new RectInt(0, 0, width1, height1);
-        room2 = new RectInt(0, 24, width2, height2);
-        room3 = new RectInt(0, 0, width1, height1);
-        room4 = new RectInt(49, 0, width2, height2);
+        int numberOfRooms = Random.Range(1, maxRooms + 1);
+        RectInt[] rooms = new RectInt[numberOfRooms];
+
+        for (int i = 0; i < numberOfRooms; i++)
+        {
+            int width = Random.Range(minRoomSize, 100);
+            int height = Random.Range(minRoomSize, 50);
+            rooms[i] = new RectInt(0, 0, width, height);
+        }
+
+        if (numberOfRooms > 0)
+        {
+            room1 = rooms[0];
+        }
+        else
+        {
+            room1 = new RectInt(0, 0, 0, 0);
+        }
+
+        if (numberOfRooms > 1)
+        {
+            room2 = rooms[1];
+        }
+        else
+        {
+            room2 = new RectInt(0, 0, 0, 0);
+        }
+
+        if (numberOfRooms > 2)
+        {
+            room3 = rooms[2];
+        }
+        else
+        {
+            room3 = new RectInt(0, 0, 0, 0);
+        }
+
+        if (numberOfRooms > 3)
+        {
+            room4 = rooms[3];
+        }
+        else
+        {
+            room4 = new RectInt(0, 0, 0, 0);
+        }
     }
 
     void Update()
---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Graph<T>
+{
+    private Dictionary<T, List<T>> adjacencyList;
+
+    public Graph()
+    {
+        adjacencyList = new Dictionary<T, List<T>>();
+    }
+
+    public void AddNode(T node)
+    {
+        if (!adjacencyList.ContainsKey(node))
+        {
+            adjacencyList[node] = new List<T>();
+        }
+    }
+
+    public void AddEdge(T fromNode, T toNode)
+    {
+        if (!adjacencyList.ContainsKey(fromNode) || !adjacencyList.ContainsKey(toNode))
+        {
+            Debug.Log("One or both nodes do not exist in the graph.");
+            return;
+        }
+        adjacencyList[fromNode].Add(toNode);
+        adjacencyList[toNode].Add(fromNode);
+    }
+}
