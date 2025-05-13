6e787658522d902da133920d6a1795d3cbbe3066
Infernich <pentagonvisdax@gmail.com>
2025-05-13 15:15:22 +0200

    Better generation

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -2,25 +2,29 @@ using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    public bool splitHorizontally = false;
+    public int minRoomSize = 25;
+
+    private RectInt room1, room2, room3, room4;
+
+    void Start()
+    {
+        GenerateRooms();
+    }
+
+    void GenerateRooms()
+    {
+        int width1 = Random.Range(minRoomSize, 100);
+        int width2 = Random.Range(minRoomSize, 100);
+        int height1 = Random.Range(minRoomSize, 50);
+        int height2 = Random.Range(minRoomSize, 50);
+        room1 = new RectInt(0, 0, width1, height1);
+        room2 = new RectInt(0, 24, width2, height2);
+        room3 = new RectInt(0, 0, width1, height1);
+        room4 = new RectInt(49, 0, width2, height2);
+    }
 
     void Update()
     {
-        RectInt room1, room2, room3, room4;
-        if (splitHorizontally)
-        {
-            room1 = new RectInt(0, 0, 100, 25);
-            room2 = new RectInt(0, 24, 100, 26);
-            room3 = new RectInt(0, 0, 50, 25);
-            room4 = new RectInt(49, 0, 51, 25);
-        }
-        else
-        {
-            room1 = new RectInt(0, 0, 50, 50);
-            room2 = new RectInt(49, 0, 51, 50);
-            room3 = new RectInt(0, 0, 50, 25);
-            room4 = new RectInt(0, 24, 50, 26);
-        }
         AlgorithmsUtils.DebugRectInt(room1, Color.green);
         AlgorithmsUtils.DebugRectInt(room2, Color.blue);
         AlgorithmsUtils.DebugRectInt(room3, Color.yellow);
