217151da03f2acb92543be646f5371a9056db027
Infernich <pentagonvisdax@gmail.com>
2025-05-13 14:45:33 +0200

    Warm up 3
    Split horizontal or vertical via boolean

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    RectInt room1 = new RectInt(0, 0, 50, 50);
-    RectInt room2 = new RectInt(49, 0, 51, 50);
+    public bool splitHorizontally = false;
 
     void Update()
     {
+        RectInt room1, room2;
+        if (splitHorizontally)
+        {
+            room1 = new RectInt(0, 0, 100, 25);
+            room2 = new RectInt(0, 24, 100, 26);
+        }
+        else
+        {
+            room1 = new RectInt(0, 0, 50, 50);
+            room2 = new RectInt(49, 0, 51, 50);
+        }
         AlgorithmsUtils.DebugRectInt(room1, Color.green);
         AlgorithmsUtils.DebugRectInt(room2, Color.blue);
     }
